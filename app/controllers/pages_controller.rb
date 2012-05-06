3b2d5b8f1c1372fce4a395260dfcd80e7de999ca
Young Woong Park <ywpark90@gmail.com>
2012-05-06 19:51:45 +0900

    Done with content layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,25 @@
 class PagesController < ApplicationController
   def about
-  end
+ 		@title_img_src = "about/about_title.png"
+	end
 
   def resume
+ 		@title_img_src = "resume/resume_title.png"
   end
 
   def projects
+ 		@title_img_src = "about/about_title.png"
   end
 
   def blog
+ 		@title_img_src = "about/about_title.png"
   end
 
   def music
+ 		@title_img_src = "about/about_title.png"
   end
 
   def contact
+ 		@title_img_src = "about/about_title.png"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Return a logo image tag
+	def logo
+		image_tag("menu/logo.png", :alt => "Logo", :id => "menu_logo")
+	end
+
+	# Return a title image tag
+	def title_img
+		image_tag("#{@title_img_src}", :alt => "Title", :id => "content_title")
+	end
+
 end

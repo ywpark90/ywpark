3632b0fad7b58c14d2cd3af3ad8813b0f35e100b
Young Woong Park <ywpark90@gmail.com>
2012-05-07 15:19:12 +0900

    Done with resume layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,18 +8,18 @@ class PagesController < ApplicationController
   end
 
   def projects
- 		@title_img_src = "about/about_title.png"
+ 		@title_img_src = "projects/projects_title.png"
   end
 
   def blog
- 		@title_img_src = "about/about_title.png"
+ 		@title_img_src = "blog/blog_title.png"
   end
 
   def music
- 		@title_img_src = "about/about_title.png"
+ 		@title_img_src = "music/music_title.png"
   end
 
   def contact
- 		@title_img_src = "about/about_title.png"
+ 		@title_img_src = "contact/contact_title.png"
   end
 end
